57dc1d795bbc162ed6ff71850b4a34760fc2cfba
VNZ1nxz <125399839+vnz1nx@users.noreply.github.com>
2024-12-10 14:52:42 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Encontre o Castelo!",
-  description: "Jogo de aventura.",
+  title: "Vans",
+  description: "Site da Vans.",
 };
 
 export default function RootLayout({ children }) {
