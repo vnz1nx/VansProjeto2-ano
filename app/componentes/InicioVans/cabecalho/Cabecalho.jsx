ec8f817cb089c32d7462e94735576504f0ba9dde
VNZ1nx <125399839+vnz1nx@users.noreply.github.com>
2025-02-07 11:44:21 -0300

    Add files via upload

--- app/componentes/InicioVans/cabecalho/Cabecalho.jsx
+++ app/componentes/InicioVans/cabecalho/Cabecalho.jsx
@@ -1,89 +1,105 @@
+'use client';
+import { useEffect } from 'react';
 import './cabecalho.css'
 
 export default function Cabecalho(){
+    useEffect(() => {
+        const menu = document.querySelector(".menu-toggle");
+        const cabecalho = document.querySelector(".cabecalho");
+    
+        if (window.innerWidth <= 768) {
+            cabecalho.classList.add("mobile");
+        }
+    
+        // menu.addEventListener("click", () => {
+        //     sidebar.classList.toggle("collapsed");
+        // });
+    }, []);
+    
+
     return(
         <div className='cabecalho'>
-            <div className='pesquisa'>
-                <div className='logo'></div>
-                <input type="search" className='search' placeholder="Old Skooll..."/>
-                <select>
-                    <option>MAIS</option>
-                    <option>CALÇAS</option>
-                    <option>BLUSAS</option>
-                    <option>TENIS</option>
-                </select>
-                <a href="#" className='Ajuda'>AJUDA</a>
-                <a href="#" className='Lojas'>LOJAS</a>
-                <a href="#" className='Conta'>CONTA</a>
-            </div>
-
-            <ul className="opcoes">
-                <li className="dropdown" id="Web">
-                    <span>WEB</span>
-                    <div className="dropdown-content">
-                        <a href="#">Aplicativo</a><br />
-                        <a href="#">Contato</a><br />
-                        <a href="#">Lojas Fisicas</a>
+                    <div className='pesquisa'>
+                        <div className='logo'></div>
+                        <input type="search" className='search' placeholder="Old Skooll..."/>
+                        <select>
+                            <option>MAIS</option>
+                            <option>CALÇAS</option>
+                            <option>BLUSAS</option>
+                            <option>TENIS</option>
+                        </select>
+                        <a href="#" className='Ajuda'>AJUDA</a>
+                        <a href="#" className='Lojas'>LOJAS</a>
+                        <a href="#" className='Conta'>CONTA</a>
                     </div>
-                </li>
 
-                <li className="dropdown" id="Novidades">
-                    <span>NOVIDADES</span>
-                    <div className="dropdown-content">
-                        <a href="#">Blusas</a><br />
-                        <a href="#">Calças</a><br />
-                        <a href="#">Tenis</a><br />
-                        <a href="#">Conjutos</a>
-                    </div>
-                </li>
+                    <ul className="opcoes">
+                        <li className="dropdown" id="Web">
+                            <span>WEB</span>
+                            <div className="dropdown-content">
+                                <a href="#">Aplicativo</a><br />
+                                <a href="#">Contato</a><br />
+                                <a href="#">Lojas Fisicas</a>
+                            </div>
+                        </li>
 
-                <li className="dropdown" id="Kids">
-                    <span>KIDS</span>
-                    <div className="dropdown-content">
-                        <a href="#">Tenis</a><br />
-                        <a href="#">Brindes</a><br />
-                        <a href="#">Premios</a>
-                    </div>
-                </li>
+                        <li className="dropdown" id="Novidades">
+                            <span>NOVIDADES</span>
+                            <div className="dropdown-content">
+                                <a href="#">Blusas</a><br />
+                                <a href="#">Calças</a><br />
+                                <a href="#">Tenis</a><br />
+                                <a href="#">Conjutos</a>
+                            </div>
+                        </li>
 
-                <li className="dropdown" id="Feminino">
-                    <span>FEMININO</span>
-                    <div className="dropdown-content">
-                        <a href="#">Blusas</a><br />
-                        <a href="#">Calças</a><br />
-                        <a href="#">Tenis</a><br />
-                        <a href="#">Vestidos</a>
-                    </div>
-                </li>
+                        <li className="dropdown" id="Kids">
+                            <span>KIDS</span>
+                            <div className="dropdown-content">
+                                <a href="#">Tenis</a><br />
+                                <a href="#">Brindes</a><br />
+                                <a href="#">Premios</a>
+                            </div>
+                        </li>
 
-                <li className="dropdown" id="Masculino">
-                    <span>MASCULINO</span>
-                    <div className="dropdown-content">
-                        <a href="#">Blusas</a><br />
-                        <a href="#">Calças</a><br />
-                        <a href="#">Tenis</a><br />
-                        <a href="#">Jaquetas</a>
-                    </div>
-                </li>
+                        <li className="dropdown" id="Feminino">
+                            <span>FEMININO</span>
+                            <div className="dropdown-content">
+                                <a href="#">Blusas</a><br />
+                                <a href="#">Calças</a><br />
+                                <a href="#">Tenis</a><br />
+                                <a href="#">Vestidos</a>
+                            </div>
+                        </li>
 
-                <li className="dropdown" id="Latest">
-                    <span>LATEST</span>
-                    <div className="dropdown-content">
-                        <a href="#">Contratos</a><br />
-                        <a href="#">Funcionarios</a><br />
-                        <a href="#">Lojas</a>
-                    </div>
-                </li>
+                        <li className="dropdown" id="Masculino">
+                            <span>MASCULINO</span>
+                            <div className="dropdown-content">
+                                <a href="#">Blusas</a><br />
+                                <a href="#">Calças</a><br />
+                                <a href="#">Tenis</a><br />
+                                <a href="#">Jaquetas</a>
+                            </div>
+                        </li>
 
-                <li className="dropdown" id="Sale">
-                    <span>SALE</span>
-                    <div className="dropdown-content">
-                        <a href="#">Contratos</a><br />
-                        <a href="#">Peças</a><br />
-                        <a href="#">Lojas</a>
-                    </div>
-                </li>
-            </ul>
+                        <li className="dropdown" id="Latest">
+                            <span>LATEST</span>
+                            <div className="dropdown-content">
+                                <a href="#">Contratos</a><br />
+                                <a href="#">Funcionarios</a><br />
+                                <a href="#">Lojas</a>
+                            </div>
+                        </li>
+
+                        <li className="dropdown" id="Sale">
+                            <span>SALE</span>
+                            <div className="dropdown-content">
+                                <a href="#">Contratos</a><br />
+                                <a href="#">Peças</a><br />
+                                <a href="#">Lojas</a>
+                            </div>
+                        </li>
+                    </ul>
         </div>
     )
 }
